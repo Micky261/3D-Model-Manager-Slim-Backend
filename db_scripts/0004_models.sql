7d20a97c629d6324b4bf063dc21f2f9380421d05
Frank Nelles <f.nelles@hotmail.com>
2022-04-01 00:04:40 +0200

    Update models table: auto update updated_at column

--- db_scripts/0004_models.sql
+++ db_scripts/0004_models.sql
@@ -1,21 +1,21 @@
 CREATE TABLE `models`
 (
-    `id`                   bigint(20) UNSIGNED NOT NULL,
-    `user_id`              bigint(20) UNSIGNED NOT NULL,
-    `name`                 varchar(255)        NOT NULL DEFAULT '',
-    `imported_name`        varchar(255)                 DEFAULT NULL,
-    `description`          text                         DEFAULT NULL,
-    `imported_description` text                         DEFAULT NULL,
-    `notes`                text                         DEFAULT NULL,
-    `links`                longtext                     DEFAULT NULL CHECK (json_valid(`links`)),
-    `favorite`             tinyint(1)          NOT NULL DEFAULT 0,
-    `author`               varchar(255)        NOT NULL DEFAULT '',
-    `imported_author`      varchar(255)                 DEFAULT NULL,
-    `licence`              varchar(255)                 DEFAULT NULL,
-    `imported_licence`     varchar(255)                 DEFAULT NULL,
-    `import_source`        varchar(255)                 DEFAULT NULL,
-    `created_at`           timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`           timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `id`                   bigint(20) UNSIGNED                   NOT NULL,
+    `user_id`              bigint(20) UNSIGNED                   NOT NULL,
+    `name`                 varchar(255)                          NOT NULL DEFAULT '',
+    `imported_name`        varchar(255)                                   DEFAULT NULL,
+    `description`          text                                           DEFAULT NULL,
+    `imported_description` text                                           DEFAULT NULL,
+    `notes`                text                                           DEFAULT NULL,
+    `links`                longtext                                       DEFAULT NULL CHECK (json_valid(`links`)),
+    `favorite`             bool                                  NOT NULL DEFAULT false,
+    `author`               varchar(255)                          NOT NULL DEFAULT '',
+    `imported_author`      varchar(255)                                   DEFAULT NULL,
+    `licence`              varchar(255)                                   DEFAULT NULL,
+    `imported_licence`     varchar(255)                                   DEFAULT NULL,
+    `import_source`        varchar(255)                                   DEFAULT NULL,
+    `created_at`           timestamp                             NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`           timestamp ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE `models`
